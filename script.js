a78e3c9f02cee870d6b408d4c33618cc2cfca1a9
Shaun <shaunszeyh@gmail.com>
2021-11-18 15:27:56 +0800

    Updated index.html, style.css and script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let movesLeft = 5;
 let playerScore = 0;
 let computerScore = 0;
+let message;
 
 function computerPlay() {
     let moves = ["rock", "paper", "scissors"];
@@ -9,53 +9,49 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    if (playerScore == 5 || computerScore == 5) {
+        playerScore = 0;
+        computerScore = 0;
+    } 
     if (playerSelection == computerSelection) {
-        return `It's a tie. You both picked ${playerSelection}!
-Score: ${playerScore} - ${computerScore}`;
+        message = `It's a tie. You both picked ${playerSelection}!`;
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore += 1;
-        movesLeft -= 1;
-        return `You win this round! Rock beats scissors!
-Score: ${playerScore} - ${computerScore}`;
+        message = `You win this round! Rock beats scissors!`;
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         computerScore += 1;
-        movesLeft -= 1;
-        return `You lose this round! Paper beats rock!
-Score: ${playerScore} - ${computerScore}`;
+        message = `You lose this round! Paper beats rock!`;
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         playerScore += 1;
-        movesLeft -= 1;
-        return `You win this round! Scissors beats paper!
-Score: ${playerScore} - ${computerScore}`;
+        message =  `You win this round! Scissors beats paper!`;
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         computerScore += 1;
-        movesLeft -= 1;
-        return `You lose this round! Rock beats scissors!
-Score: ${playerScore} - ${computerScore}`;
+        message = `You lose this round! Rock beats scissors!`;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         playerScore += 1;
-        movesLeft -= 1;
-        return `You win this round! Paper beats rock!
-Score: ${playerScore} - ${computerScore}`;
+        message = `You win this round! Paper beats rock!`;
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         computerScore += 1;
-        movesLeft -= 1;
-        return `You lose this round! Scissors beats paper!
-Score: ${playerScore} - ${computerScore}`;
-    } else {
-        return "You've entered your move wrongly...";
+        message = `You lose this round! Scissors beats paper!`;
     }
+    if (playerScore == 5) {
+        message = "You win! Congratulations! Click any button to play again.";
+    } else if (computerScore == 5) {
+        message = "You lost. Don't worry, you can play again by clicking any button.";
+    }
+    document.querySelector('.score p').textContent = `${playerScore} - ${computerScore}`;
+    document.querySelector('.message p').textContent = message;
 }
 
-function game() {
-    while (movesLeft > 0) {
-        alert(playRound(prompt("Enter either rock, paper or scissors: ").toLowerCase(), computerPlay()));
-    }
-    if (playerScore > computerScore) {
-        return `You win the game with a score of ${playerScore} - ${computerScore}!`
-    } else {
-        return `You lose the game with a score of ${playerScore} - ${computerScore}`
-    }
+document.getElementById('rock').onclick = function() {
+    playRound('rock', computerPlay());
+}
+document.getElementById('paper').onclick = function() {
+    playRound('paper', computerPlay());
+}
+document.getElementById('scissors').onclick = function() {
+    playRound('scissors', computerPlay());
 }
 
-alert(game());
+
+
